9e7b568de87ff73ef6869554b1bb5c031810a403
malick tkr <malick.tounkara.1@gmiail.com>
2021-12-29 02:29:45 +0000

    pmd template step 1 : 80%

--- public/js/util.js
+++ public/js/util.js
@@ -1,18 +1,26 @@
 var mybutton = document.getElementById("btn-scroll");
 
-        // When the user scrolls down 20px from the top of the document, show the button
-        window.onscroll = function () { scrollFunction() };
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
 
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                mybutton.style.display = "block";
-            } else {
-                mybutton.style.display = "none";
-            }
-        }
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+        } else {
+        mybutton.style.display = "none";
+    }
+}
 
-        // When the user clicks on the button, scroll to the top of the document
-        function topFunction() {
-            document.body.scrollTop = 0; // For Safari
-            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-        }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//loader
+ window.onload = function () {
+    var loader = document.getElementById('loader-pmd')
+    loader.style.display = 'none'
+}
+//year
+document.getElementById("year").innerHTML = new Date().getFullYear();
